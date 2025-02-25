04dbda1dd312b6813f551d7146d199c509948b61
VinBaz <67991028+vinaybaswa@users.noreply.github.com>
2025-02-25 10:58:49 -0500

    create route "/api/v1/tours" and test get and post

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+
+app.use(express.json());
+
+// app.get("/", (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: "Hello from the server side", app: "Natours" });
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    dtat: {
+      tours,
+    },
+  });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  newTour = Object.assign({id: newId}, req.body);
+  // console.log(newTour);
+  tours.push(newTour);
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+    res.status(201).json({
+      status: "success",
+      data: {
+        tour: newTour
+      }
+    })
+  })
+  // res.send("Done");
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Natours App is running on port ${port}`);
+});
