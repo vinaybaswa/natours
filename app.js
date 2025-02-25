b275fc59f164fbed6b259be841d476145eb1b43a
VinBaz <67991028+vinaybaswa@users.noreply.github.com>
2025-02-25 11:20:31 -0500

    Get tour with ID

--- app.js
+++ app.js
@@ -25,21 +25,43 @@ app.get("/api/v1/tours", (req, res) => {
   });
 });
 
-app.post("/api/v1/tours", (req, res) => {
-  // console.log(req.body);
-  const newId = tours[tours.length - 1].id + 1;
-  newTour = Object.assign({id: newId}, req.body);
-  // console.log(newTour);
-  tours.push(newTour);
-  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-    res.status(201).json({
-      status: "success",
+app.get("/api/v1/tours/:id", (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: "fail",
       data: {
-        tour: newTour
-      }
-    })
-  })
-  // res.send("Done");
+        message: "Ivalid ID",
+      },
+    });
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour,
+    },
+  });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
