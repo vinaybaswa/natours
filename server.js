94ac0dc3c5ef29e71b5088cc21ff94fcda4f898d
VinBaz <67991028+vinaybaswa@users.noreply.github.com>
2025-03-07 14:49:18 -0500

    create remote DB on Atlas install mongoose and connect to the db

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    // eslint-disable-next-line no-console
+    console.log("DB connection successful");
+  })
+  .catch((err) => {
+    // eslint-disable-next-line no-console
+    console.log(err);
+  });
 
 const app = require("./app");
 
