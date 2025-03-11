62f4abbb4ce1271f9f7b136dd5a7ae89f0bad815
VinBaz <67991028+vinaybaswa@users.noreply.github.com>
2025-03-11 15:50:01 -0400

    getTour with mongoose findById

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,27 +1,46 @@
 const Tour = require("../models/tourModel");
 
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: "success",
-    // results: tours.length,
-    // dtat: {
-    //   tours,
-    // },
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+
+    res.status(200).json({
+      status: "success",
+      results: tours.length,
+      dtat: {
+        tours,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "fail",
+      message: error,
+    });
+  }
 };
 
-exports.getTour = (req, res) => {
-  // res.status(200).json({
-  //   status: "success",
-  //   data: {
-  //     tour,
-  //   },
-  // });
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "fail",
+      message: error,
+    });
+  }
 };
 
 exports.createTour = async (req, res) => {
   try {
     const newTour = await Tour.create(req.body);
+
     res.status(201).json({
       status: "success",
       data: {
