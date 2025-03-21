02e6f63226fe8386fcb8f0510ccca8778fec34cb
VinBaz <67991028+vinaybaswa@users.noreply.github.com>
2025-03-21 12:10:16 -0400

    document middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const toursSchema = new mongoose.Schema(
   {
@@ -7,6 +8,7 @@ const toursSchema = new mongoose.Schema(
       required: [true, "A tour must have a name"],
       unique: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, "A tour must have a duration"],
@@ -61,6 +63,17 @@ const toursSchema = new mongoose.Schema(
 toursSchema.virtual("durationWeeks").get(function () {
   return this.duration / 7;
 });
+
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
+toursSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.post("save", function (doc, next) {
+//   console.log(doc);
+//   next();
+// });
 
 const Tour = mongoose.model("Tour", toursSchema);
 
