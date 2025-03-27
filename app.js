f28016fcd1449a8c0b4c1c25f4ee2359cddaac0b
VinBaz <67991028+vinaybaswa@users.noreply.github.com>
2025-03-27 16:01:03 -0400

    refactor error handling into errorController

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
 
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
@@ -23,22 +25,10 @@ app.use(express.static(`${__dirname}/public`));
 
 // Error handling
 app.all("*", (req, res, next) => {
-  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-  err.statusCode = 404;
-  err.status = "fail";
-
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
 // Global error handling middleware
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
