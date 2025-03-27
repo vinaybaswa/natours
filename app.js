82a01d5578b7b32469d9f618f1de643c5cf292d9
VinBaz <67991028+vinaybaswa@users.noreply.github.com>
2025-03-27 15:38:02 -0400

    Global error handling middleware

--- app.js
+++ app.js
@@ -23,11 +23,22 @@ app.use(express.static(`${__dirname}/public`));
 
 // Error handling
 app.all("*", (req, res, next) => {
-  res.status(404).json({
-    status: "fail",
-    message: `Can't find ${req.originalUrl} on this server!`,
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.statusCode = 404;
+  err.status = "fail";
+
+  next(err);
+});
+
+// Global error handling middleware
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
-  next();
 });
 
 module.exports = app;
